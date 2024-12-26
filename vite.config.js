677a4e6e7f57a0ae3def85bebc50774d3e7ee5d9
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-12-26 15:52:11 -0300

    chore(config): enhance Vite configuration with manual chunking and minification

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
@@ -6,6 +7,24 @@ export default defineConfig({
   root: './',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+          return null;
+        },
+      },
+    },
+    minify: 'esbuild',
   },
   plugins: [
     eslint({
