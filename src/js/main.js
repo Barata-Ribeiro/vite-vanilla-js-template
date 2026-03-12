789a4685633d4278287706b6ef56076a8503fc7d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-03-12 15:45:06 -0300

    chore: update favicon and enhance layout with hero image

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import '../styles/style.css';
 
 import viteLogo from '../assets/images/vite.svg';
 import javascriptLogo from '../assets/images/javascript.svg';
+import heroImg from '../assets/images/hero.png';
 
 const dependencies = [
   'ESlint',
@@ -16,20 +17,25 @@ const dependencies = [
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
+    <div id="center">
+      <a class="hero" href="https://vitejs.dev" target="_blank">
+        <img src="${heroImg}" class="base" width="170" height="179">
+        <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
+        <img src="${viteLogo}" class="vite" alt="Vite logo" />
+      </a>
+    </div>
+
     <h1>Vanilla Vite!</h1>
     <h2>Template by <cite><a class="author" href="https://github.com/Barata-Ribeiro" target="_blank" rel="noopener noreferrer">Barata-Ribeiro</a></cite></h2>
+    
     <div class="card">
       <button id="counter" type="button"></button>
       <a class="gitRepo" href="https://github.com/Barata-Ribeiro/vite-vanilla-js-template" target="_blank" rel="noopener noreferrer">Repository</a>
     </div>
+    
     <div class="tags">
     </div>
+
     <p class="read-the-docs">
       Click on the Vite logo to learn more about Vite.js.
     </p>
